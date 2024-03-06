548491e8a23de943d0e6db2ddbe9e8e92ba6462e
Linecom-lik <3448336734@qq.com>
2024-03-07 01:07:05 +0800

    Initial Commit

---
+++ Linecom Wear BMI Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Linecom Wear BMI Watch App
+//
+//  Created by 澪空 on 2024/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Linecom Wear BMI Watch App/Linecom_Wear_BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Linecom_Wear_BMIApp.swift
+//  Linecom Wear BMI Watch App
+//
+//  Created by 澪空 on 2024/3/7.
+//
+
+import SwiftUI
+
+@main
+struct Linecom_Wear_BMI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
